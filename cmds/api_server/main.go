2a86d6b2cfe309c7562e3c463663685e755171ab
karn <mail@karngyan.com>
2024-10-03 08:09:22 +0530

    mise run dev to start local server

--- cmds/api_server/main.go
+++ cmds/api_server/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+  "net/http"
+  "time"
+
+  "github.com/karngyan/maek/conf"
+  "github.com/karngyan/maek/routers"
+
+  "github.com/beego/beego/v2/server/web"
+  "github.com/beego/beego/v2/server/web/filter/cors"
+)
+
 func main() {
-	
+
+  web.AddAPPStartHook(
+    routers.Init,
+    conf.Init,
+    // db.Init,
+    // models.Init,
+  )
+
+  allowedOrigins := conf.CorsAllowedOrigins
+  if conf.IsDevEnv() {
+    allowedOrigins = []string{"localhost"}
+  }
+
+  web.InsertFilter("*", web.BeforeRouter, cors.Allow(&cors.Options{
+    AllowOrigins:     allowedOrigins,
+    AllowCredentials: true,
+    MaxAge:           24 * time.Hour,
+    AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions, http.MethodPatch},
+    AllowHeaders:     []string{"Content-Type", "Authorization", "Origin"},
+    ExposeHeaders:    []string{"Content-Length"},
+  }), web.WithReturnOnOutput(false))
+
+  web.Run("localhost")
 }
---
+++ conf/conf.go
@@ -0,0 +1,57 @@
+package conf
+
+import (
+  "fmt"
+  "runtime"
+  "time"
+
+  "github.com/beego/beego/v2/server/web"
+  "github.com/pkg/errors"
+)
+
+// NoStatus is a placeholder when there's no valid HTTP status
+// to return with a request
+const NoStatus = -1
+
+// DefaultRequestTimeout is the maximum TTL for individual requests
+const DefaultRequestTimeout = 10 * time.Second
+
+// DefaultMaxRead sets a cap on how much data we will read from an individual response
+const DefaultMaxRead = 1024 * 1024 // 1MB
+
+var (
+  SQLConn            string
+  RedisAddr          string
+  CorsAllowedOrigins []string
+)
+
+func IsDevEnv() bool {
+  return web.BConfig.RunMode == "dev"
+}
+
+func IsProdEnv() bool {
+  return web.BConfig.RunMode == "prod"
+}
+
+func Init() error {
+
+  var err error
+
+  if SQLConn, err = web.AppConfig.String("SQLConn"); err != nil {
+    return errors.WithStack(err)
+  }
+
+  if RedisAddr, err = web.AppConfig.String("RedisAddr"); err != nil {
+    return errors.WithStack(err)
+  }
+
+  CorsAllowedOrigins = web.AppConfig.DefaultStrings("CorsAllowedOrigins", []string{"*"})
+
+  return err
+}
+
+// MakeUserAgent creates a standard webhook User-Agent string
+// to help us better identify bad actors
+func MakeUserAgent(id string) string {
+  return fmt.Sprintf("maek.ai/%v (%v; +https://maek.ai/actions)", runtime.Version(), id)
+}
