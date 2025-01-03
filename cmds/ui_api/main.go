9978628d85cd903bf9599459b0227544b74bcb19
karn <mail@karngyan.com>
2025-01-03 20:45:31 +0530

    import sorts

--- cmds/ui_api/main.go
+++ cmds/ui_api/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"github.com/karngyan/maek/db"
-	"github.com/karngyan/maek/domains"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxevent"
 	"go.uber.org/zap"
 
 	"github.com/karngyan/maek/config"
+	"github.com/karngyan/maek/db"
+	"github.com/karngyan/maek/domains"
 	"github.com/karngyan/maek/libs/logger"
 	"github.com/karngyan/maek/ui_api"
 )
