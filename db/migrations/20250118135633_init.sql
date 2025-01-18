fc832be709a385eacb41f924f47219fcab81d576
Anamika <43814685+anamikak005@users.noreply.github.com>
2025-01-18 21:26:02 +0530

    ch: pg vector exists by default (#8)

--- db/migrations/20250118135633_init.sql
+++ db/migrations/20250118135633_init.sql
@@ -1,7 +1,5 @@
 -- +goose up
 -- +goose statementbegin
-CREATE EXTENSION IF NOT EXISTS vector;
-
 -- workspace table
 CREATE TABLE IF NOT EXISTS workspace
 (
@@ -142,10 +140,7 @@ DROP TABLE IF EXISTS collection;
 DROP TABLE IF EXISTS note;
 DROP TABLE IF EXISTS session;
 DROP TABLE IF EXISTS user_workspaces;
-ALTER TABLE "user"
-    DROP CONSTRAINT IF EXISTS fk_user_default_workspace;
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS workspace;
 
-DROP EXTENSION IF EXISTS vector;
 -- +goose statementend
--- makefile
+++ makefile
@@ -5,10 +5,10 @@ help: ## Display this help
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 up: ## Start all docker services
-	docker-compose up -d
+	docker compose up -d
 
 down: ## Stop all docker services
-	docker-compose down
+	docker compose down
 
 dev: ## Start development server
 	air
