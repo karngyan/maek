064430d1464c40e5472f7451581a5a72899a874b
karn <mail@karngyan.com>
2025-01-24 04:25:36 +0530

    outline hide on add to your notes

--- ui/components/quick-create/inp/index.tsx
+++ ui/components/quick-create/inp/index.tsx
@@ -62,7 +62,7 @@ const QuickCreateInp = () => {
           name='quick-note'
           rows={2}
           placeholder='add to your notes ...'
-          className='caret-zinc-400 block w-full resize-none border-0 bg-transparent py-1.5 text-zinc-300 placeholder:text-zinc-600 focus:ring-0 text-sm leading-6'
+          className='caret-zinc-400 block w-full resize-none border-0 bg-transparent px-3 outline-hidden py-2 text-zinc-300 placeholder:text-zinc-600 focus:ring-0 text-sm leading-6'
           defaultValue={''}
           onInput={handleInput}
         />
